89088fd73afd7efd2caa79675ada58d5d6347742
Cristian Hasna <cristian.hasna@gmail.com>
2020-03-21 11:36:50 +0200

    Updated config and main.ts to output result into txt files

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+import { EntryGenerator } from './generators/EntryGenerator.ts';
+import { Entry } from './model/Entry.ts';
+import { PublicationField } from './model/PublicationField.ts';
+import { SubscriptionField } from './model/SubscriptionField.ts';
 import { GeneratorConfig } from './types/GeneratorConfig.ts';
 
 (async () => {
@@ -8,9 +12,27 @@ import { GeneratorConfig } from './types/GeneratorConfig.ts';
   const fileContent = await Deno.readFile(configFile);
   const decoder = new TextDecoder('utf-8');
   const config: GeneratorConfig = JSON.parse(decoder.decode(fileContent));
-  
-  for(const field of config.model) {
-    console.log(field);
+
+  const entryGenerator = new EntryGenerator();
+  for (const field of config.model) {
+    entryGenerator.addField(field);
   }
 
+  const publications: Entry<PublicationField>[] = [];
+  const subscriptions: Entry<SubscriptionField>[] = [];
+
+  for (let i = 0; i < config.entriesCount; i++) {
+    publications.push(entryGenerator.generatePublication());
+    subscriptions.push(entryGenerator.generateSubscription());
+  }
+
+  const encoder = new TextEncoder();
+  Deno.writeFile(
+    config.output.publications,
+    encoder.encode(publications.map((publication) => publication.toString()).join('\n'))
+  );
+  Deno.writeFile(
+    config.output.subscriptions,
+    encoder.encode(subscriptions.map((publication) => publication.toString()).join('\n'))
+  );
 })();
